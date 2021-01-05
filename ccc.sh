0004305bcc0b5a73c074fdaa341548a724c420a7
changqi97 <changqi97@gmail.com>
2021-01-05 23:21:47 +0800

    add wait.py

--- ccc.sh
+++ ccc.sh
@@ -17,7 +17,7 @@ mkdir /data/$1_$2/
 gdc-client download -m $1_$2.txt -d /data/$1_$2
 python ~/gdc_downloader/shellupload/wait.py --dir $1 --num $2
 cd ~
-mkdir /onedrive/DX/$1_$2
-./rclone copy /data/$1_$2 /onedrive/DX/$1_$2
+mkdir /onedrive/DX_test/$1_$2
+./rclone copy /data/$1_$2 /onedrive/DX_test/$1_$2
 rm -rf /data/$1_$2
 echo "$1_$2_download_upload_done"
