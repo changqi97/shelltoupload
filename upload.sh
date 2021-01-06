3f7dbcf08f69da7300eb9c2dde9073dae032f205
齐畅 <qichang@qichangdeMac-mini.local>
2021-01-06 19:23:52 +0800

    update test

--- upload.sh
+++ upload.sh
@@ -4,13 +4,11 @@
  # @Author: Chang Qi
  # @Date: 2021-01-06 11:24:59
  # @LastEditors: Chang Qi
- # @LastEditTime: 2021-01-06 14:18:27
+ # @LastEditTime: 2021-01-06 19:23:32
  # @Email: changqi97@gmail.com
 ### 
 cd ~
 mkdir onedrive/DX_new/$1_$2
 echo "upload begin"
 ./rclone copy /data/$1_$2 /onedrive/DX_new/$1_$2
-echo "upload done"
-rm -rf /data/$1_$2
 echo "$1_$2_download_upload_done"
