6fc33c3402cb2e7d3084857974acb0f0d2d63bde
齐畅 <qichang@qichangdeMac-mini.local>
2021-01-06 20:38:27 +0800

    update test

--- split.py
+++ split.py
@@ -4,7 +4,7 @@ version:
 Author: Chang Qi
 Date: 2021-01-05 15:44:57
 LastEditors: Chang Qi
-LastEditTime: 2021-01-06 14:23:18
+LastEditTime: 2021-01-06 20:38:19
 Email: changqi97@gmail.com
 '''
 
@@ -19,7 +19,7 @@ args = parser.parse_args()
 # _,txtname = os.path.split(args.dir)
 os.makedirs('../DX_dir/{}'.format(args.dir),exist_ok=True)
 
-with open('../DX/{}.txt'.format(txtname),'r')as f:
+with open('../DX/{}.txt'.format(args.dir),'r')as f:
     lines = f.readlines()
     for index,line in enumerate(lines,1):
         with open('../DX_dir/{}/{}_{}.txt'.format(args.dir,args.dir,index%3),'a+')as tmp:
